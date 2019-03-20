51708a317413c74e76dca2cc46c1897ea3ab76f4
sky <1019722231@qq.com>
2019-03-20 23:54:58 +0800

    sdf

--- study_v1/src/main/java/Test.java
+++ study_v1/src/main/java/Test.java
@@ -9,20 +9,10 @@ public class Test {
         System.out.println("3sdf23");
         System.out.println("323");
             System.out.println("dsdf");
+            System.out.println("dsdf");
         System.out.println("3223r");
         System.out.println("32ds3");
-            System.out.println("dsdf");
-        System.out.println("888");
-        System.out.println("32d3");
-      System.out.println("323");
-        System.out.println("sd");
-            System.out.println("43");
-        System.out.println("323");
 
-        System.out.println("323");
-        System.out.println("sd");
-            System.out.println("43");
-        System.out.println("323");
 
     }
 }
